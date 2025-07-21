3d92895d948ad772ca10f378b7da72b33c6ef62a
youlethal <78674543+youlethal@users.noreply.github.com>
2025-07-22 00:17:13 +0100

    Add src/types/index.ts

---
+++ src/types/index.ts
@@ -0,0 +1,13 @@
+export interface BusinessConfig {
+  name: string
+  category: string
+  template: string
+  modules: string[]
+  theme: string
+}
+
+export interface ContactForm {
+  name: string
+  email: string
+  message: string
+}
